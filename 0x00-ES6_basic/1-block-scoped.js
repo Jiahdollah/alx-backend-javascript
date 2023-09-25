f5f3ecda4163a0174d196b91b3471778e4e84f81
peter njihia <peternjihia2001@gmail.com>
2023-09-25 20:45:46 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true; // Use 'let' instead of 'var'
 
   if (trueOrFalse) {
-    task = true; // Do not re-declare, just assign the value
-    task2 = false; // Do not re-declare, just assign the value
+  let task = true; // Do not re-declare, just assign the value
+  let task2 = false; // Do not re-declare, just assign the value
   }
 
   return [task, task2];
