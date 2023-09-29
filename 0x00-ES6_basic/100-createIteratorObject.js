2efc3980ea273eef518efc77d5e326e605bfc81c
peter njihia <peternjihia2001@gmail.com>
2023-09-29 22:26:57 +0000

    iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+#take argument
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...item,
+    ];
+  }
+  return allEmployees;
+}
