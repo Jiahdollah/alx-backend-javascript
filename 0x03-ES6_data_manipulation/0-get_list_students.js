bb1ba91d892d9a0e235e345a9b7f0540398431ba
peter njihia <peternjihia2001@gmail.com>
2023-10-06 18:25:42 +0000

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,7 @@
-function getListStudents() {
-let id: 1, firstName: 'Guillaume', location: 'San Francisco',
-    id: 2, firstName: 'James', location: 'Columbia',
-    id: 5, firstName: 'Serena', location: 'San Francisco'
-return [id (Number), firstName (String), and location (String)]
-}
-console.log(getListStudents())
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
 
-
+export default getListStudents;
