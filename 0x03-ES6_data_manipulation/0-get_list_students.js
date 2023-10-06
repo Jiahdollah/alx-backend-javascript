00c0470e5fe9376af7021b39b6e66348e3f9567c
peter njihia <peternjihia2001@gmail.com>
2023-10-06 16:36:24 +0000

    basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+function getListStudents() {
+let id: 1, firstName: 'Guillaume', location: 'San Francisco',
+    id: 2, firstName: 'James', location: 'Columbia',
+    id: 5, firstName: 'Serena', location: 'San Francisco'
+return [id (Number), firstName (String), and location (String)]
+}
+console.log(getListStudents())
+
+
